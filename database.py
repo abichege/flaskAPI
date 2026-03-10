b1314bbe1f636b3efef33c80541b7d519b894944
abichege <liquidambish@gmail.com>
2026-03-10 21:27:29 +0300

    initial

--- database.py
+++ database.py
@@ -13,3 +13,15 @@ class User(Base):
     id: Mapped[int]=mapped_column(primary_key=True)
     name: Mapped[str]=mapped_column(String(100))
     location: Mapped[str]=mapped_column(String(100))
+
+# from flask_sqlalchemy import SQLAlchemy
+
+# db=SQLAlchemy()
+
+# class User(db.Model):
+#     __tablename__='users'
+#     id=db.Column(db.Integer,primary_key=True)
+#     username=db.Column(db.String(50),nullable=False)
+#     email=db.Column(db.String(120),unique=True,nullable=False)
+#     password=db.Column(db.String(55),nullable=False)
+
--- main.py
+++ main.py
@@ -5,10 +5,29 @@
 # 3.You must define a http metthod. E.g-GET,POST,PUT,DELETE,PATCH
 # 4.You must define a status code E.g-200,201,404,401,500
 from flask import Flask, jsonify, request
+from sqlalchemy import create_engine,select
+from sqlalchemy.orm import sessionmaker
+from database import User,Base
+from flask_cors import CORS
 
 app = Flask(__name__)
 
-allowed_methods = ["GET", "POST", "PUT", "DELETE"]
+CORS(app)
+# , origins=["http://127.0.0.1:5500"]
+
+DATABASE_URL = "postgresql+psycopg2://postgres:blossomabigael@localhost:5432/vue_myduka"
+
+# connecting SQLAlchemy to PSQL using engine function
+engine = create_engine(DATABASE_URL, echo=False)
+
+# create session to call query methods
+session = sessionmaker(bind=engine)
+my_session=session()
+
+# create tables automatically
+Base.metadata.create_all(engine)
+
+allowed_methods = ["GET", "POST", "PUT", "DELETE","PATCH"]
 user_list = []
 
 
@@ -26,17 +45,52 @@ def users():
     try:
         method = request.method.lower()
         if method == "get":
+            user_list = []
+            query=select(User)
+            my_users=list(my_session.scalars(query).all())
+            # print(users)
+
+            for user in my_users:
+                user_list.append({"id":user.id,
+                                "name":user.name,
+                                "location":user.location})
+
             return jsonify({"data": user_list}), 200
         elif method == "post":
+            # convert json to dictionary
             data = request.get_json()
+            # check if all fields are received
             if data["name"] == "" or data["location"] == "":
                 return jsonify({"msg": "All fields required"}), 401
             else:
-                user_list.append(data)
+                # user_list.append(data)/store user in users tables using SQLAlchemypip
+                new_user = User(name=data["name"], location=data["location"])
+                my_session.add(new_user)
+                my_session.commit()
+                my_session.close()
+
                 return jsonify({"msg": "Successfully added user"}), 201
         else:
             return jsonify({"msg": "Method not allowed"}), 405
     except Exception as e:
-        return jsonify({"error":str(e)}),500
+        return jsonify({"error": str(e)}), 500 
+    
+@app.route('/register',methods=allowed_methods)   
+def register():
+    # convert json to dictionary
+            data = request.get_json()
+            # check if all fields are received
+            if data["username"] == "" or data["email"] == "" or data["password"] == "":
+                return jsonify({"msg": "All fields required"}), 401
+            else:
+                # user_list.append(data)/store user in users tables using SQLAlchemypip
+                new_user = User(username=data["username"], email=data["email"],password=data["password"])
+                my_session.add(new_user)
+                my_session.commit()
+                my_session.refresh(new_user)
+
+                return jsonify({"msg": "Successfully added user"}), 201
+
+
 
 app.run(debug=True)
